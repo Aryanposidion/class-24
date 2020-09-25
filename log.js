d1f1d0df03626456f7dc5fb924dfe511fd192341
Aryanposidion <70718363+Aryanposidion@users.noreply.github.com>
2020-09-25 17:00:27 -0400

    Add files via upload

---
+++ log.js
@@ -0,0 +1,28 @@
+class log {
+    constructor(x, y, height,angle) {
+      var options = {
+          'restitution':0.8,
+          'friction':1.5,
+          'density':1.0
+      }
+      this.body = Bodies.rectangle(x, y, 20, height, options);
+      this.width = 20;
+      Matter.Body.setAngle(this.body,angle)
+      this.height = height;
+      
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      var angle=this.body.angle
+      push();
+      strokeWeight(3)
+      stroke("blue")
+      translate(pos.x,pos.y);
+      rotate(angle)
+      rectMode(CENTER);
+      fill("purple");
+      rect(0, 0, this.width, this.height);
+      pop();
+    }
+  };
